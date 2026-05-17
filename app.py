a6b625f9e0e22aabab4ee6a9d7026bd1f4919329
giovannadsr <giovanna.sr@sempreceub.com>
2026-05-17 17:02:27 -0300

    fix: configure Flask app for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from src.weather import obter_clima
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,6 @@ def clima():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
+
